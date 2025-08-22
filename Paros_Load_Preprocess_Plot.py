68fbd343534b88ac7a5523ab7ce32ced062da20a
Yi Zhang <58002595+yizhang24@users.noreply.github.com>
2025-08-21 19:12:34 -0700

    Apply synthesis to load preprocess plot

--- Paros_Load_Preprocess_Plot.py
+++ Paros_Load_Preprocess_Plot.py
@@ -28,6 +28,7 @@ Date:   2025-08-17
 from pathlib import Path
 from paros_data_grabber import query_influx_data, save_data
 from Preprocessing_fun import preprocess, welch_psd
+from Wind_Synthesis_Util import addWindNoise
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
@@ -58,7 +59,7 @@ if not data:
     raise RuntimeError("No data returned from InfluxDB.")
 else:
     # Convert to NumPy array
-    data_array = np.array(data['parost2_141929'])
+    data_array = np.array(data['parost2_141929'][["time", "value"]])
     # save_data(data, str(output_file))
     # print(f"Data saved to: {output_file.resolve()}")
 
@@ -69,18 +70,24 @@ unix_times = data_array[:, 0]
 dt_utc = np.array([datetime.fromtimestamp(t, tz=timezone.utc) for t in unix_times])
 raw_waveform = np.asarray(data_array[:, 1]).flatten()
 
+noise_added = addWindNoise(raw_waveform, fs)
+
 # Preprocess the waveform
 signal = preprocess(raw_waveform, fs)
+signal2 = preprocess(noise_added, fs)
 
 # Compute PSD
 pxx, f_psd= welch_psd(signal, fs)
+pxx2, f_psd2= welch_psd(signal2, fs)
 
 # -------------------------------
 # --- Plotting Functions ---
 # -------------------------------
-def plot_signal(signal, dt):
+def plot_signal(dt, signal, signal2=None):
     plt.figure()
     plt.plot(dt, signal, linewidth=1.5)
+    plt.plot(dt, signal2, linewidth=1.5, label="Wind added")
+    plt.legend()
     plt.xlabel('Time (UTC)')
     plt.ylabel('Pressure (mB)')
     plt.title('Preprocessed Time Series Data')
@@ -88,9 +95,11 @@ def plot_signal(signal, dt):
     plt.tight_layout()
     plt.show()
 
-def plot_psd(frequencies, power_spectral_density):
+def plot_psd(frequencies, power_spectral_density, frequencies2=None, power_spectral_density_2 = None):
     plt.figure()
     plt.plot(frequencies, 10*np.log10(power_spectral_density), linewidth=1.2)
+    plt.plot(frequencies2, 10*np.log10(power_spectral_density_2), linewidth=1.2, label="Wind added")
+    plt.legend()
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Power/Frequency (dB/Hz)')
     plt.title('Welch Power Spectral Density')
@@ -98,7 +107,7 @@ def plot_psd(frequencies, power_spectral_density):
     plt.tight_layout()
     plt.show()
 
-def plot_spectrogram(signal, fs, window_length=64, shift=8, nfft=1024):
+def plot_spectrogram(signal, fs, signal2 = None, window_length=64, shift=8, nfft=1024):
     overlap = window_length - shift
     f_spect, t_spect, Sxx = spectrogram(signal, fs, window=hann(window_length),
                                         nperseg=window_length, noverlap=overlap, nfft=nfft)
@@ -115,6 +124,6 @@ def plot_spectrogram(signal, fs, window_length=64, shift=8, nfft=1024):
 # -------------------------------
 # --- Execute Plots ---
 # -------------------------------
-plot_signal(signal, dt_utc)
-plot_psd(f_psd, pxx)
+plot_signal(dt_utc, signal, signal2)
+plot_psd(f_psd, pxx, f_psd2, pxx2)
 plot_spectrogram(signal, fs)
